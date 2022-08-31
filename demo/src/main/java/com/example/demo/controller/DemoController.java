2e372584b8f813c4b2aecc10e850c9e72572056f
xiao.li <m42m42m42@163.com>
2022-08-31 18:02:06 +0800

    refactor.

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -11,32 +11,28 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/employees")
 public class DemoController {
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping("/")
-    public String hello() {
-        return "hello";
-    }
-
-    @GetMapping("/employees/all")
+    @GetMapping("/all")
     public EmployeeResp all() {
         return employeeService.all();
     }
 
-    @PostMapping("employees/addition")
+    @PostMapping("/addition")
     @ResponseStatus(value = HttpStatus.CREATED)
     public boolean add(@RequestBody EmployeeReq req) {
         return employeeService.add(req);
     }
 
-    @GetMapping("employees/{id}")
+    @GetMapping("/{id}")
     public Employee getEmployeeById(@PathVariable(name = "id") Long employeeId) {
         return employeeService.findById(employeeId);
     }
 
-    @GetMapping("employees")
+    @GetMapping
     public List<Employee> getEmployeesByGender(@RequestParam(value = "gender") String gender) {
         return employeeService.findByGender(gender);
     }
