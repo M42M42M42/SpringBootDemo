51d1e94d23911f0972132d0f591779d023722780
xiao.li <m42m42m42@163.com>
2022-08-31 17:42:14 +0800

    add all() and add() and getEmployeeById() in EmployeeController.

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,12 +1,36 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.controller.request.EmployeeReq;
+import com.example.demo.controller.responce.EmployeeResp;
+import com.example.demo.entity.Employee;
+import com.example.demo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class DemoController {
+    @Autowired
+    private EmployeeService employeeService;
+
     @GetMapping("/")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/employees/all")
+    public EmployeeResp all() {
+        return employeeService.all();
+    }
+
+    @PostMapping("employees/addition")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public boolean add(@RequestBody EmployeeReq req) {
+        return employeeService.add(req);
+    }
+
+    @GetMapping("employees/{id}")
+    public Employee getEmployeeById(@PathVariable(name = "id") Long employeeId) {
+        return employeeService.findById(employeeId);
     }
 }
